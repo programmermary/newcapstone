380f48eba0f8cb2cbf793ac25432aadeb426f87e
Maryam Ihsan <maryamihsan601@gmail.com>
2024-06-15 14:35:45 +0300

    Update HomePageCards.jsx

--- src/components/Cards/HomePageCards.jsx
+++ src/components/Cards/HomePageCards.jsx
@@ -6,15 +6,15 @@ export default function HomePageCards() {
     <div className='flex flex-col justify-center items-center w-full h-full pt-[10px] '>
       <div className='flex justify-evenly  items-baseline w-[90%] h-1/2 rounded-[20px] my-2 bg-[#2E335B] opacity-[70%]'>
           <h1 className='text-[40px] text-white self-center'>خدماتنا</h1>
-          <img className='w-[20%] h-3/4' src={services}/>
+          <img className='w-[20%] h-3/4' src={services} alt="serviceimage"/>
       </div>
       <div className='flex justify-evenly  items-baseline w-[90%] h-1/2 rounded-[20px] my-2 bg-[#2E335B] opacity-[70%]'>
           <h1 className='text-[40px] text-white self-center'>رعاية مستمرة</h1>
-          <img className='w-[20%] h-3/4' src={services}/>
+          <img className='w-[20%] h-3/4' src={services} alt=""/>
       </div>
       <div className='flex justify-evenly  items-baseline w-[90%] h-1/2 rounded-[20px] my-2 bg-[#2E335B] opacity-[70%]'>
           <h1 className='text-[40px] text-white self-center'>فرقنا الطبية</h1>
-          <img className='w-[20%] h-3/4' src={services}/>
+          <img className='w-[20%] h-3/4' src={services} alt=""/>
       </div>
     </div>
   )
