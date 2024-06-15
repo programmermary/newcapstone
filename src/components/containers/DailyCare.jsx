8a4c874aff191c1c3e82f8ec334a4337ab14a8c8
Maryam Ihsan <maryamihsan601@gmail.com>
2024-06-15 14:37:51 +0300

    Update DailyCare.jsx

--- src/components/containers/DailyCare.jsx
+++ src/components/containers/DailyCare.jsx
@@ -10,7 +10,7 @@ import user from '../../assets/user.png'
 export default function DailyCare() {
   return (
     <div className='form-div h-full'>
-      <img className=' h-44 w-44 mt-20 mb-20 mr-16' src={user}/>
+      <img className=' h-44 w-44 mt-20 mb-20 mr-16' src={user} alt=""/>
        
        
     <div className='image bg-gray-300 w-full h-1/2 rounded-full flex justify-center items-center'>
